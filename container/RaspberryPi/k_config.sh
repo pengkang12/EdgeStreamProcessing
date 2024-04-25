13da7dbbb1b142b531b9535f96f64b3096a469d1
Peng Kang <123kangpeng@gmail.com>
2024-04-25 14:55:38 -0500

    update

--- container/RaspberryPi/k_config.sh
+++ container/RaspberryPi/k_config.sh
@@ -3,11 +3,13 @@
 # specify kubernetes' version you want to install.
 version=1.24
 
+mkdir /etc/apt/keyrings/
+
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v${version}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${version}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt update
+apt update
 
 echo "apt install -y kubeadm=${version}.1-1.1 kubelet=${version}.1-1.1 kubectl=${version}.1-1.1 "
 
