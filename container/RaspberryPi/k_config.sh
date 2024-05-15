9a9f8216432296b1a435d1f2a200c9f55f539296
Peng <123kangpeng@gmail.com>
2024-05-15 13:28:11 -0500

    Update k_config.sh
    Update flannel network

--- container/RaspberryPi/k_config.sh
+++ container/RaspberryPi/k_config.sh
@@ -45,18 +45,12 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-# install cluster networking
-wget https://github.com/flannel-io/flannel/releases/download/v0.19.2/flanneld-arm64
-sudo chmod +x flanneld-arm64
-sudo cp flanneld-arm64 /usr/local/bin/flanneld
-sudo mkdir -p /var/lib/k8s/flannel/networks
-
 # 
 sudo swapoff -a
 
 echo "-------------------------------------------------------------"
 echo "please run the following command to build kubernete cluster"
 echo "sudo systemctl restart containerd.service"
-echo "sudo kubeadm init  --pod-network-cidr 192.168.0.0/16 --ignore-preflight-errors=... |sudo  tee /var/log/kubeinit.log"
+echo "sudo kubeadm init  --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=... |sudo  tee /var/log/kubeinit.log"
 echo "Build the network cluster for kubernetes"
 echo "kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml"
